c345f65d29a1f49972d33e2329c907b3a572ec1d
derrck <dcdengchao@gmail.com>
2014-01-20 19:51:02 +0800

    2014-01-20

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
 /* none */
+var book = {
+	_year: 2004,
+	edition: 1
+};
+
+object.defineProperty(book, "year", {
+	get: function(){
+		return this._year;
+	},
+	set: function(newValue){
+		if(newValue > 2004) {
+			this._year = newValue;
+			this.edition += newValue - 2004;
+		}
+	}
+});
+
+book.year=2005;
+alert(book.edition);
