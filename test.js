1f602ef9f63099e6bda95e32859b9ddd78e43ec6
derrck <dcdengchao@gmail.com>
2014-03-01 10:39:13 +0800

    1

--- test.js
+++ test.js
@@ -1,25 +1,14 @@
-/* none */
-<<<<<<< HEAD
-var book = {
-	_year: 2004,
-	edition: 1
-};
+var pageWidth = window.innerWidth,
+	pageHeight = window.innerHeight;
 
-object.defineProperty(book, "year", {
-	get: function(){
-		return this._year;
-	},
-	set: function(newValue){
-		if(newValue > 2004) {
-			this._year = newValue;
-			this.edition += newValue - 2004;
-		}
+if (typeof pageWidth != 'number') {
+	if (document.compatMode == "CSS1Compat") {
+			pageWidth = document.documentElement.clientWidth;
+			pageHeight = document.documentElement.clientHeight;
+	
+	}else{
+		pageWidth = document.body.clientWidth;
+		pageHeight = document.body.clientHeight;
 	}
-});
-
-book.year=2005;
-alert(book.edition);
-=======
-alert(test);
->>>>>>> 49d5f1c95cb5bae1fbcaffb21a681106c7869dfb
-alert("111");
+}
+alert(pageWidth);
